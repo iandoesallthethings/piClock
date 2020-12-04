01ab81d701bedc96520e1bbf11828bb4e0df30b2
Ian Edwards <denkyuu@gmail.com>
2020-12-04 16:50:01 -0500

    Refactor alarm and timer, add dev script.

--- clock.py
+++ clock.py
@@ -1,19 +1,18 @@
 # -*- coding:utf-8 -*-
+import RPi.GPIO as GPIO
 import SH1106
-import time
 import config
 import traceback
-import datetime
-import subprocess
-import humanize
-from textwrap3 import wrap
-
-import RPi.GPIO as GPIO
-
 
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
+
+import time
+import datetime
+import humanize
+from textwrap3 import wrap
+
 
 #GPIO define
 RST_PIN        = 25
@@ -35,9 +34,6 @@ KEY3_PIN       = 16
 disp = SH1106.SH1106()
 disp.Init()
 
-# Clear display.
-disp.clear()
-# time.sleep(1)
 
 #init GPIO
 # for P4:
@@ -52,8 +48,11 @@ GPIO.setup(KEY_PRESS_PIN,   GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull
 GPIO.setup(KEY1_PIN,        GPIO.IN, pull_up_down=GPIO.PUD_UP)      # Input with pull-up
 GPIO.setup(KEY2_PIN,        GPIO.IN, pull_up_down=GPIO.PUD_UP)      # Input with pull-up
 GPIO.setup(KEY3_PIN,        GPIO.IN, pull_up_down=GPIO.PUD_UP)      # Input with pull-up
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
+
+# Clear display.
+disp.clear()
+
+# Create blank image for drawing with mode '1' for 1-bit color.
 image = Image.new('1', (disp.width, disp.height), "WHITE")
 
 # Get drawing object to draw on image.
@@ -62,116 +61,150 @@ font = ImageFont.truetype('Font.ttf', 40)
 font2 = ImageFont.truetype('Font.ttf', 16)
 font10 = ImageFont.truetype('Font.ttf', 13)
 fontTiny = ImageFont.truetype('Font.ttf', 11)
+# Import BMPs
 bell = Image.open("./bell.bmp").convert("1")
 stopwatch = Image.open("./timer.bmp").convert("1")
 
-# App State
-refreshRate = 0.1 # in seconds
+# Helpers
+def showClock(_):
+    global view
+    view = Clock
 
-currentDisplay = 'timer'
-alarmSet = True
-timerRunning = True
-timerStart = datetime.datetime.now()
-timer = datetime.timedelta(seconds = 10)
+def showTimer(_):
+    global view
+    view = Timer
 
-# Input Handling
-def setToClockMode(channel):
-    print("Switching to Clock Mode")
-    global currentDisplay
-    currentDisplay = 'clock'
+def chopMicroseconds(delta):
+    return delta - datetime.timedelta(microseconds=delta.microseconds)
 
-def setToTimerMode(channel):
-    print("Switching to Timer Mode")
-    global currentDisplay
-    currentDisplay = 'timer'
-
-
-def toggle(channel):
-    if currentDisplay == 'clock':
-        toggleAlarm()
-    elif currentDisplay == 'timer':
-        toggleTimer()
-
-def toggleAlarm():
-    print("Toggling Alarm")
-    global alarmSet
-    alarmSet = not alarmSet
-
-def toggleTimer():
-    global timerRunning, timerStart
-    timerStart = datetime.datetime.now()
-    timerRunning = not timerRunning
-
-def up(channel):
-    global timer
-    timer += datetime.timedelta(minutes = 1)
-def down(channel):
-    global timer
-    timer -= datetime.timedelta(minutes = 1)
-def right(channel):
-    global timer
-    timer += datetime.timedelta(minutes = 5)
-def left(channel):
-    global timer
-    timer -= datetime.timedelta(minutes = 5)
-
-GPIO.add_event_detect(KEY1_PIN, GPIO.RISING, callback=setToClockMode)
-GPIO.add_event_detect(KEY2_PIN, GPIO.RISING, callback=setToTimerMode)
-GPIO.add_event_detect(KEY3_PIN, GPIO.RISING, callback=toggle)
-GPIO.add_event_detect(KEY_UP_PIN, GPIO.RISING, callback=up)
-GPIO.add_event_detect(KEY_DOWN_PIN, GPIO.RISING, callback=down)
-GPIO.add_event_detect(KEY_LEFT_PIN, GPIO.RISING, callback=left)
-GPIO.add_event_detect(KEY_RIGHT_PIN, GPIO.RISING, callback=right)
-
-
-def displayClock(time):
-    draw.text((0,-14), now.strftime("%I:%M"), font = font, fill = 0) # Time
-    draw.text((98,16), now.strftime("%P"), font = fontTiny, fill = 0) # AM/PM
-    draw.line([(30,40),(98,40)], fill = 0, width = 1) # Line
-    draw.text((0,43), now.strftime("%a %d, %h %Y"), font = font2, fill = 0) # Date
-
-def displayTimer(now):
-    global timer, timerStart
-    timeRemaining = None
-
-    if timerRunning:
-        timeElapsed = now - timerStart
-        timeRemaining = timer - timeElapsed
-        if timeRemaining <= datetime.timedelta(0):
-            timeRemaining = datetime.timedelta(0)
-    else:
-        timeRemaining = timer
-
-    for i, chunk in enumerate(formatTimerString(timeRemaining)):
-        draw.text((10,15 * i), chunk, font = font10, fill = 0)
-
-def formatTimerString(delta):
+def humanizeAndWrapTime(delta):
+    delta = chopMicroseconds(delta)
     preciseDelta = humanize.precisedelta(delta, minimum_unit = 'seconds')
-    timeString = wrap(preciseDelta, width = 15)
+    timeString = wrap(preciseDelta, width = 12)
     return timeString
 
+# Views
+class Clock:
+    alarmSet = False
+    alarmTime = datetime.time(hour = 15, minute = 27)
+    alarmRinging = False
 
-# Program Loop
+    ringInterval = 1 # in seconds
+    ringTimer = 0
+
+    def draw():
+        draw.text((0,-14), now.strftime("%I:%M"), font = font, fill = 0) # Time
+        draw.text((98,16), now.strftime("%P"), font = fontTiny, fill = 0) # AM/PM
+        draw.line([(30,40),(98,40)], fill = 0, width = 1) # Line
+        #draw.text((0,43), now.strftime("%a %d, %h %Y"), font = font2, fill = 0) # Date
+        draw.text((0,43), Clock.alarmTime.strftime("%I:%M %P"), font = font2, fill = 0) # Date
+
+    def toggle():
+        Clock.alarmSet = not Clock.alarmSet
+        Clock.alarmRinging = False
+
+    def up():
+        print('clock up')
+    def down():
+        print('clock down')
+    def right():
+        print('clock right')
+    def left():
+        print('clock left')
+
+    def checkAlarm():
+        if now.time().replace(microsecond=0) == Clock.alarmTime and Clock.AlarmSet:
+            Clock.alarmRinging = True
+
+    def ringAlarm():
+        global refreshRate
+        if Clock.ringTimer == 0: print("RING! WAKE UP!") # Ring the actual bell HERE!
+        Clock.ringTimer += refreshRate
+        if Clock.ringTimer >= Clock.ringInterval: Clock.ringTimer = 0
+
+class Timer:
+    duration = datetime.timedelta(seconds = 10)
+    start = datetime.datetime.now()
+    running = False
+    ringing = False
+
+    ringInterval = 2 # in seconds
+    ringTimer = 0 # increments by refreshrate
+    
+
+    def check():
+        if Timer.running and Timer.timeRemaining() <= datetime.timedelta(0):
+            Timer.ringing = True
+    def ring():
+        global refreshRate
+        if Timer.ringTimer <= 0: print("RING! TIMES UP!") # Ring the actual bell HERE!
+        Timer.ringTimer += refreshRate
+        if Timer.ringTimer >= Timer.ringInterval: Timer.ringTimer = 0
+
+
+    def timeRemaining():
+        timeRemaining = None
+        if Timer.running:
+            timeElapsed = now - Timer.start
+            timeRemaining = Timer.duration - timeElapsed
+        else:
+            timeRemaining = Timer.duration
+        if timeRemaining <= datetime.timedelta(0):
+            timeRemaining = datetime.timedelta(0)
+        return timeRemaining
+    
+    def draw():
+        humanizedTime = humanizeAndWrapTime(Timer.timeRemaining())
+        for i, chunk in enumerate(humanizedTime):
+            draw.text((10,15 * i), chunk, font = font10, fill = 0)
+
+    def toggle():
+        Timer.start = datetime.datetime.now()
+        Timer.running = not Timer.running
+        Timer.ringing = False
+
+    def up():
+        Timer.duration += datetime.timedelta(minutes = 1)
+    def down():
+        Timer.duration -= datetime.timedelta(minutes = 1)
+    def right():
+        Timer.duration += datetime.timedelta(minutes = 5)
+    def left():
+        Timer.duration -= datetime.timedelta(minutes = 5)
+
+# Setup
+refreshRate = 0.1 # in seconds
+bouncetime = 100 # in milliseconds
+view = Clock
+
+GPIO.add_event_detect(KEY1_PIN, GPIO.RISING, callback=showClock, bouncetime=bouncetime)
+GPIO.add_event_detect(KEY2_PIN, GPIO.RISING, callback=showTimer, bouncetime=bouncetime)
+GPIO.add_event_detect(KEY3_PIN, GPIO.RISING, callback=lambda _: view.toggle(), bouncetime=bouncetime)
+GPIO.add_event_detect(KEY_UP_PIN, GPIO.RISING, callback=lambda _: view.up(), bouncetime=bouncetime)
+GPIO.add_event_detect(KEY_DOWN_PIN, GPIO.RISING, callback=lambda _: view.down(), bouncetime=bouncetime)
+GPIO.add_event_detect(KEY_LEFT_PIN, GPIO.RISING, callback=lambda _: view.left(), bouncetime=bouncetime)
+GPIO.add_event_detect(KEY_RIGHT_PIN, GPIO.RISING, callback=lambda _: view.right(), bouncetime=bouncetime)
+
+# Main
 try:
     startTime = time.time()
     while True:
+        now = datetime.datetime.now()
+                
         draw.rectangle((0,0,disp.width,disp.height), fill=1) # Clear the frame
         
-        if alarmSet == True:
-            draw.bitmap((118,0), bell)
-        if timerRunning == True:
-            draw.bitmap((118,15), stopwatch)
-            
-        # Update the stuff
-        now = datetime.datetime.now()
+        if Clock.alarmSet: draw.bitmap((118,0), bell)
+        if Timer.running: draw.bitmap((118,15), stopwatch)
+
+        Clock.checkAlarm()
+        if Clock.alarmRinging: Clock.ringAlarm()
+
+        Timer.check()
+        if Timer.ringing: Timer.ring()
+
 
         # Display the stuff
-        if currentDisplay == 'clock':
-            displayClock(now)
-        elif currentDisplay == 'timer':
-            displayTimer(now)
-        elif currentDisplay == 'derp':
-            draw.text((10,0), 'DERP', font = font, fill = 0)
+        view.draw()
 
         # Update the display
         disp.ShowImage(disp.getbuffer(image))
---
+++ dev.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+# Setup a work space called `work` with two windows
+# first window has 3 panes. 
+# The first pane set at 65%, split horizontally, set to api root and running vim
+# pane 2 is split at 25% and running redis-server 
+# pane 3 is set to api root and bash prompt.
+# note: `api` aliased to `cd ~/path/to/work`
+#
+session="work"
+
+cd piClock
+# set up tmux
+tmux start-server
+
+# create a new tmux session, starting vim from a saved session in the new window
+tmux new-session -d -s $session
+
+# Select pane 1, set dir to api, run vim
+tmux send-keys "vim clock.py" C-m 
+
+# Split pane 1 horizontal by 65%, start nodemon
+tmux splitw -v -p 2
+tmux send-keys "nodemon clock.py" C-m 
+
+tmux selectp -1 1
+tmux splitw -h -p 50
+
+# Select pane 0 
+tmux selectp -t 0
+
+# Finished setup, attach to the tmux session!
+tmux attach-session -t $session
